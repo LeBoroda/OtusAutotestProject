8b594f66983774e038c0542786f69de6d1142e1a
Bonjour <you@example.com>
2023-02-08 21:40:22 +0300

    all tests are passed

--- src/main/java/pages/EventsPage.java
+++ src/main/java/pages/EventsPage.java
@@ -16,7 +16,7 @@ public class EventsPage extends AbsPage {
 
         long lastHeight = (long) jsExecutor.executeScript("return document.body.scrollHeight");
 
-        while (true) {
+        for (int i = 0; i < 100; i++) {
             jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight);");
             waiter.waitForCondition(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(loaderSelector)));
             waiter.waitForCondition(ExpectedConditions.not(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(loaderSelector))));
