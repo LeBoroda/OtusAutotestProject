12713f1450f4d9507ac7632ed42336e52ef07b5b
Bonjour <you@example.com>
2023-02-08 21:49:27 +0300

    all tests are passed

--- src/main/java/pages/TestingCoursesCatalogPage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class TestingCoursesCatalogPage extends AbsPage{
-    public TestingCoursesCatalogPage(WebDriver driver) {
-        super(driver, "/catalog/courses?categories=testing");
-    }
-}
